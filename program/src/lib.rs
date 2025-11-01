4e4f94ab6cf567d899cfd58863463e1e46e1c62a
lucasfishh <arkmmhompulvia@hotmail.com>
2025-11-01 21:25:02 +0000

    Extend native Solana program scaffold

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -8,3 +8,13 @@ pub mod state;
 
 use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+#[cfg(not(feature = "no-entrypoint"))]
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
