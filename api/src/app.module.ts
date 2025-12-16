f07af61cea141304cd0f961297cf0b686a16f1e4
lucasfishh <arkmmhompulvia@hotmail.com>
2025-12-16 19:06:52 +0000

    Wire API runtime configuration and module wiring

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,3 +3,12 @@ import { DbService } from './db/db.service';
 import { RegistryService } from './registry/registry.service';
 import { RegistryController } from './registry/registry.controller';
 import { ConfigController } from './config/config.controller';
+import { RpcController } from './rpc/rpc.controller';
+import { HeartbeatController } from './heartbeat/heartbeat.controller';
+import { IndexerService } from './indexer/indexer.service';
+
+@Module({
+  controllers: [ConfigController, RegistryController, RpcController, HeartbeatController],
+  providers: [DbService, RegistryService, IndexerService],
+})
+export class AppModule {}
