d2d6b4a853ee7b13369c01880a92c2dc62772fbb
lucasfishh <arkmmhompulvia@hotmail.com>
2026-01-26 11:41:08 +0000

    Finish Solana account indexer polling

--- api/src/indexer/indexer.service.ts
+++ api/src/indexer/indexer.service.ts
@@ -3,3 +3,8 @@ import { Connection, PublicKey } from '@solana/web3.js';
 import { CONFIG } from '../config';
 import { DbService } from '../db/db.service';
 import { RegistryService } from '../registry/registry.service';
+
+const TAG_NAME = 2;
+
+// Decoded on-chain name record. Layout must match program/src/state.rs.
+function decodeNameRecord(data: Buffer) {
