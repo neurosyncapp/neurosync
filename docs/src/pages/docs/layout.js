3bd9a741ccae262f8c1c3c269e16d9a416e56c5e
lucasfishh <arkmmhompulvia@hotmail.com>
2026-05-17 12:17:52 +0000

    Wire documentation router and layout

--- docs/src/pages/docs/layout.js
+++ docs/src/pages/docs/layout.js
@@ -8,3 +8,13 @@ import { renderResolution } from './content/protocol/resolution.js';
 import { renderRegisterHandle } from './content/developers/register-handle.js';
 import { renderHeartbeat } from './content/developers/heartbeat.js';
 import { renderResolverApi } from './content/developers/resolver-api.js';
+import { renderOnChain } from './content/reference/on-chain.js';
+import { renderFaq } from './content/reference/faq.js';
+
+export const docPages = {
+  introduction: {
+    title: 'Introduction',
+    content: renderIntroduction,
+    toc: [
+      { id: 'what-is-neurosync', title: 'What is NeuroSync?' },
+      { id: 'the-gap', title: 'The gap it fills' },
