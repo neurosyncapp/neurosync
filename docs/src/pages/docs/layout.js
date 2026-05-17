90a66efb3fd192d46fffabce6fa6b76fcc76be79
lucasfishh <arkmmhompulvia@hotmail.com>
2026-05-17 11:18:07 +0000

    Add documentation router and layout

---
+++ docs/src/pages/docs/layout.js
@@ -0,0 +1,10 @@
+import { renderIntroduction } from './content/getting-started/introduction.js';
+import { renderCoreConcepts } from './content/getting-started/core-concepts.js';
+import { renderQuickstart } from './content/getting-started/quickstart.js';
+import { renderHandles } from './content/protocol/handles.js';
+import { renderPresence } from './content/protocol/presence.js';
+import { renderReputation } from './content/protocol/reputation.js';
+import { renderResolution } from './content/protocol/resolution.js';
+import { renderRegisterHandle } from './content/developers/register-handle.js';
+import { renderHeartbeat } from './content/developers/heartbeat.js';
+import { renderResolverApi } from './content/developers/resolver-api.js';
